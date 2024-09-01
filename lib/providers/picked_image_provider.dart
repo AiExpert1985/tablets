1621d91810dececf4066041c535a9ac53dcb199a
Shaheen <AiExpert2025@gmail.com>
2024-09-01 13:24:56 +0300

    ...

---
+++ lib/providers/picked_image_provider.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final firebaseAuthProvider = Provider<File>((ref){
+  return FirebaseAuth.instance;
+});
