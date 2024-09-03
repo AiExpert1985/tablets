5efbf707b598a7d2bc6800d291c238fffd0b89d7
Shaheen <AiExpert2025@gmail.com>
2024-09-03 13:49:03 +0300

    ...

--- lib/src/features/authentication/presentation/widgets/login/login_screen.dart
+++ lib/src/features/authentication/presentation/widgets/login/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:tablets/generated/l10n.dart';
 import 'package:tablets/src/features/authentication/repository/auth_repository.dart';
 
