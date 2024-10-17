0365c7902c1fb2e005cd1dbf0b804cea01adc6a6
shaheen <aiexpert2025@gmail.com>
2024-10-17 22:45:29 +0300

    successfuly make FormInputField globally usable

---
+++ lib/src/common/widgets/input_form_field.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tablets/generated/l10n.dart';
+import 'package:tablets/src/common/constants/constants.dart';
+import 'package:tablets/src/common/functions/utils.dart' as utils;
+import 'package:tablets/src/common/functions/form_validation.dart' as validation;
+
+class InputFormField extends ConsumerWidget {
+  const InputFormField({
+    required this.formDataUpdateFn,
+    required this.dataType,
+    required this.name,
+    required this.displayedTitle,
+    this.initialValue,
+    super.key,
+  });
+
+  final String? initialValue;
+  final void Function({required String key, required String value}) formDataUpdateFn;
+  final FieldDataTypes dataType;
+  final String name;
+  final String displayedTitle;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Expanded(
+      child: FormBuilderTextField(
+        name: name,
+        initialValue: initialValue,
+        decoration: utils.formFieldDecoration(label: displayedTitle),
+        onSaved: (value) {
+          dynamic userValue = value;
+          if (dataType == FieldDataTypes.int) {
+            userValue = int.tryParse(value!);
+          }
+          if (dataType == FieldDataTypes.double) {
+            userValue = double.tryParse(value!);
+          }
+          formDataUpdateFn(key: name, value: userValue);
+        },
+        validator: (value) {
+          if (dataType == FieldDataTypes.string) {
+            return validation.validateStringField(
+                fieldValue: value,
+                errorMessage: S.of(context).input_validation_error_message_for_strings);
+          }
+          if (dataType == FieldDataTypes.int) {
+            return validation.validateIntField(
+                fieldValue: value,
+                errorMessage: S.of(context).input_validation_error_message_for_integers);
+          }
+          if (dataType == FieldDataTypes.double) {
+            return validation.validateDoubleField(
+                fieldValue: value,
+                errorMessage: S.of(context).input_validation_error_message_for_doubles);
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
--- lib/src/features/categories/view/category_form_fields.dart
+++ lib/src/features/categories/view/category_form_fields.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tablets/generated/l10n.dart';
-import 'package:tablets/src/common/constants/constants.dart' as constants;
-import 'package:tablets/src/common/functions/utils.dart' as utils;
-import 'package:tablets/src/common/functions/form_validation.dart' as validation;
+import 'package:tablets/src/common/constants/constants.dart';
+import 'package:tablets/src/common/widgets/input_form_field.dart';
 import 'package:tablets/src/features/categories/controllers/category_form_fields_data_provider.dart';
 
 class CategoryFormFields extends ConsumerWidget {
@@ -13,68 +11,17 @@ class CategoryFormFields extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ref.watch(categoryFormFieldsDataProvider);
-    return GeneralFormField(
-      dataType: constants.FieldDataTypes.string.name,
-      name: 'name',
-      displayedTitle: S.of(context).product_name,
-    );
-  }
-}
-
-class GeneralFormField extends ConsumerWidget {
-  const GeneralFormField({
-    required this.dataType,
-    required this.name,
-    required this.displayedTitle,
-    super.key,
-  });
-
-  final String dataType;
-  final String name;
-  final String displayedTitle;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final userFormData = ref.watch(categoryFormFieldsDataProvider);
-    dynamic initialValue = userFormData[name];
-
+    final categoryFormController = ref.watch(categoryFormFieldsDataProvider.notifier);
+    dynamic initialValue = categoryFormController.getState()['name'];
     if (initialValue != null) {
       initialValue = initialValue is String ? initialValue : initialValue.toString();
     }
-    return Expanded(
-      child: FormBuilderTextField(
-        name: name,
-        initialValue: initialValue,
-        decoration: utils.formFieldDecoration(label: displayedTitle),
-        onSaved: (value) {
-          dynamic userValue = value;
-          if (dataType == constants.FieldDataTypes.int.name) {
-            userValue = int.tryParse(value!);
-          }
-          if (dataType == constants.FieldDataTypes.double.name) {
-            userValue = double.tryParse(value!);
-          }
-          ref.read(categoryFormFieldsDataProvider.notifier).update(key: name, value: userValue);
-        },
-        validator: (value) {
-          if (dataType == constants.FieldDataTypes.string.name) {
-            return validation.validateStringField(
-                fieldValue: value,
-                errorMessage: S.of(context).input_validation_error_message_for_strings);
-          }
-          if (dataType == constants.FieldDataTypes.int.name) {
-            return validation.validateIntField(
-                fieldValue: value,
-                errorMessage: S.of(context).input_validation_error_message_for_integers);
-          }
-          if (dataType == constants.FieldDataTypes.double.name) {
-            return validation.validateDoubleField(
-                fieldValue: value,
-                errorMessage: S.of(context).input_validation_error_message_for_doubles);
-          }
-          return null;
-        },
-      ),
+    return InputFormField(
+      formDataUpdateFn: categoryFormController.update,
+      initialValue: initialValue,
+      dataType: FieldDataTypes.string,
+      name: 'name',
+      displayedTitle: S.of(context).product_name,
     );
   }
 }
