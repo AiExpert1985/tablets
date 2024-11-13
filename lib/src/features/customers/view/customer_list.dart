2b2243031052f9c46c78297b83766a050b332110
shaheen <aiexpert2025@gmail.com>
2024-11-13 14:47:22 +0300

    before changing Customer list to ConsumerStatefullWidget

--- lib/src/features/customers/view/customer_list.dart
+++ lib/src/features/customers/view/customer_list.dart
@@ -35,37 +35,11 @@ Widget buildCustomerList(BuildContext context, WidgetRef ref) {
   return AsyncValueWidget<List<Map<String, dynamic>>>(
     value: customerListValue,
     data: (customers) {
-      // Calculate totals for the columns
-      double totalDebtSum = 0;
-      int totalOpenInvoices = 0;
-      int totalDueInvoices = 0;
-      double totalDueDebtSum = 0;
-
-      for (var customerData in customers) {
-        final customer = Customer.fromMap(customerData);
-        final customerTransactions = getCustomerTransactions(_allTransactions, customer.dbRef);
-        final totalDebt = getTotalDebt(customerTransactions, customer);
-        final openInvoices = getOpenInvoices(customerTransactions, totalDebt);
-        final dueInvoices = getDueInvoices(openInvoices, customer.paymentDurationLimit);
-        final dueDebt = getDueDebt(dueInvoices, 4);
-
-        totalDebtSum += totalDebt;
-        totalOpenInvoices += openInvoices.length;
-        totalDueInvoices += dueInvoices.length;
-        totalDueDebtSum += dueDebt;
-      }
-
       return Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            // const Divider(),
-            // Pass totals to the header row
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: _buildHeaderRow(
-                  context, totalDebtSum, totalOpenInvoices, totalDueInvoices, totalDueDebtSum),
-            ),
+            _buildHeaderRow(context, customers),
             const Divider(),
             _buildDataRows(customers, formDataNotifier, imagePickerNotifier)
           ],
@@ -91,11 +65,8 @@ Widget _buildDataRows(List<Map<String, dynamic>> customers, ItemFormData formDat
         final matchingList = customerMatching(customerTransactions, customer, ctx);
         return Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 6.0),
-              child: _buildDataRow(customer, ctx, imagePickerNotifier, formDataNotifier, totalDebt,
-                  openInvoices, dueInvoices, dueDebt, statusColor, matchingList),
-            ),
+            _buildDataRow(customer, ctx, imagePickerNotifier, formDataNotifier, totalDebt,
+                openInvoices, dueInvoices, dueDebt, statusColor, matchingList),
             const Divider(thickness: 0.2, color: Colors.grey)
           ],
         );
@@ -104,39 +75,60 @@ Widget _buildDataRows(List<Map<String, dynamic>> customers, ItemFormData formDat
   );
 }
 
-Widget _buildHeaderRow(BuildContext context, double totalDebtSum, int totalOpenInvoices,
-    int totalDueInvoices, double totalDueDebtSum) {
-  return Column(
-    children: [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(child: _buildHeader('')),
-          Expanded(child: _buildHeader(S.of(context).customer)),
-          Expanded(child: _buildHeader(S.of(context).salesman_selection)),
-          Expanded(child: _buildHeader(S.of(context).current_debt)),
-          Expanded(child: _buildHeader(S.of(context).num_open_invoice)),
-          Expanded(child: _buildHeader(S.of(context).num_due_invoices)),
-          Expanded(child: _buildHeader(S.of(context).due_debt_amount)),
-        ],
-      ),
-      VerticalGap.m,
-      Visibility(
-        visible: showColumnTotals,
-        child: Row(
+Widget _buildHeaderRow(BuildContext context, List<Map<String, dynamic>> customers) {
+  // Calculate totals for the columns
+  double totalDebtSum = 0;
+  int totalOpenInvoices = 0;
+  int totalDueInvoices = 0;
+  double totalDueDebtSum = 0;
+
+  for (var customerData in customers) {
+    final customer = Customer.fromMap(customerData);
+    final customerTransactions = getCustomerTransactions(_allTransactions, customer.dbRef);
+    final totalDebt = getTotalDebt(customerTransactions, customer);
+    final openInvoices = getOpenInvoices(customerTransactions, totalDebt);
+    final dueInvoices = getDueInvoices(openInvoices, customer.paymentDurationLimit);
+    final dueDebt = getDueDebt(dueInvoices, 4);
+
+    totalDebtSum += totalDebt;
+    totalOpenInvoices += openInvoices.length;
+    totalDueInvoices += dueInvoices.length;
+    totalDueDebtSum += dueDebt;
+  }
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      children: [
+        Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Expanded(child: SizedBox()), // Placeholder for the first column
-            const Expanded(child: SizedBox()), // Placeholder for the second column
-            const Expanded(child: SizedBox()), // Placeholder for the third column
-            Expanded(child: _buildHeader('(${numberToText(totalDebtSum)})')),
-            Expanded(child: _buildHeader('($totalOpenInvoices)')),
-            Expanded(child: _buildHeader('($totalDueInvoices)')),
-            Expanded(child: _buildHeader('(${numberToText(totalDueDebtSum)})')),
+            Expanded(child: _buildHeader('')),
+            Expanded(child: _buildHeader(S.of(context).customer)),
+            Expanded(child: _buildHeader(S.of(context).salesman_selection)),
+            Expanded(child: _buildHeader(S.of(context).current_debt)),
+            Expanded(child: _buildHeader(S.of(context).num_open_invoice)),
+            Expanded(child: _buildHeader(S.of(context).num_due_invoices)),
+            Expanded(child: _buildHeader(S.of(context).due_debt_amount)),
           ],
         ),
-      ),
-    ],
+        VerticalGap.m,
+        Visibility(
+          visible: showColumnTotals,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Expanded(child: SizedBox()), // Placeholder for the first column
+              const Expanded(child: SizedBox()), // Placeholder for the second column
+              const Expanded(child: SizedBox()), // Placeholder for the third column
+              Expanded(child: _buildHeader('(${numberToText(totalDebtSum)})')),
+              Expanded(child: _buildHeader('($totalOpenInvoices)')),
+              Expanded(child: _buildHeader('($totalDueInvoices)')),
+              Expanded(child: _buildHeader('(${numberToText(totalDueDebtSum)})')),
+            ],
+          ),
+        ),
+      ],
+    ),
   );
 }
 
@@ -152,47 +144,50 @@ Widget _buildDataRow(
   Color color,
   List<List<dynamic>> matchingList,
 ) {
-  return Column(
-    children: [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: InkWell(
-              child: const CircleAvatar(
-                radius: 15,
-                foregroundImage: CachedNetworkImageProvider(defaultImageUrl),
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 6.0),
+    child: Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: InkWell(
+                child: const CircleAvatar(
+                  radius: 15,
+                  foregroundImage: CachedNetworkImageProvider(defaultImageUrl),
+                ),
+                onTap: () =>
+                    _showEditCustomerForm(context, formDataNotifier, imagePickerNotifier, customer),
               ),
-              onTap: () =>
-                  _showEditCustomerForm(context, formDataNotifier, imagePickerNotifier, customer),
             ),
-          ),
-          Expanded(child: _buildDataCell(customer.name, color)),
-          Expanded(child: _buildDataCell(customer.salesman, color)),
-          Expanded(
-            child: InkWell(
-              child: _buildDataCell(numberToText(totalDebt), color),
-              onTap: () => showCustomerMatchingReport(context, matchingList, customer.name),
+            Expanded(child: _buildDataCell(customer.name, color)),
+            Expanded(child: _buildDataCell(customer.salesman, color)),
+            Expanded(
+              child: InkWell(
+                child: _buildDataCell(numberToText(totalDebt), color),
+                onTap: () => showCustomerMatchingReport(context, matchingList, customer.name),
+              ),
             ),
-          ),
-          Expanded(
-            child: InkWell(
-              child: _buildDataCell(numberToText(openInvoices.length), color),
-              onTap: () => showOpenInvoicesReport(
-                  context, openInvoices, '${customer.name}  ( ${openInvoices.length} )'),
+            Expanded(
+              child: InkWell(
+                child: _buildDataCell(numberToText(openInvoices.length), color),
+                onTap: () => showOpenInvoicesReport(
+                    context, openInvoices, '${customer.name}  ( ${openInvoices.length} )'),
+              ),
             ),
-          ),
-          Expanded(
-            child: InkWell(
-              child: _buildDataCell(numberToText(dueInvoices.length), color),
-              onTap: () => showDueInvoicesReport(
-                  context, dueInvoices, '${customer.name}  ( ${dueInvoices.length} )'),
+            Expanded(
+              child: InkWell(
+                child: _buildDataCell(numberToText(dueInvoices.length), color),
+                onTap: () => showDueInvoicesReport(
+                    context, dueInvoices, '${customer.name}  ( ${dueInvoices.length} )'),
+              ),
             ),
-          ),
-          Expanded(child: _buildDataCell(numberToText(dueDebt), color)),
-        ],
-      ),
-    ],
+            Expanded(child: _buildDataCell(numberToText(dueDebt), color)),
+          ],
+        ),
+      ],
+    ),
   );
 }
 
