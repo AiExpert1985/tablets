3da056d2a8ffd9a77b0846485793b329cd21ac31
shaheen <aiexpert2025@gmail.com>
2024-12-14 22:02:41 +0300

    increase form title size

--- lib/src/common/widgets/form_title.dart
+++ lib/src/common/widgets/form_title.dart
@@ -4,6 +4,6 @@ Widget buildFormTitle(String title) {
   return Center(
     child: Text(title,
         style: const TextStyle(
-            fontSize: 20, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 184, 7, 7))),
+            fontSize: 26, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 184, 7, 7))),
   );
 }
