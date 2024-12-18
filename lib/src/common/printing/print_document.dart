257b4b0cb805659a9faecf190b3892a599addb43
shaheen <aiexpert2025@gmail.com>
2024-12-18 13:36:10 +0300

    remove landscape option from printer because it is included in pdf options

--- lib/src/common/printing/print_document.dart
+++ lib/src/common/printing/print_document.dart
@@ -21,7 +21,7 @@ const lightBgColor = PdfColor(0.85, 0.85, 0.99);
 const bordersColor = PdfColors.grey;
 const labelsColor = PdfColors.red;
 
-Future<void> _printPDf(Document pdf, int numCopies, bool isLandScape) async {
+Future<void> _printPDf(Document pdf, int numCopies) async {
   try {
     for (int i = 0; i < numCopies; i++) {
       await Printing.layoutPdf(
@@ -29,9 +29,9 @@ Future<void> _printPDf(Document pdf, int numCopies, bool isLandScape) async {
           // Return the PDF document as bytes
           return await pdf.save();
         },
-        format: isLandScape
-            ? PdfPageFormat.a4.landscape
-            : PdfPageFormat.a4.portrait, // Specify A4 landscape format
+        // format: isLandScape
+        //     ? PdfPageFormat.a4.landscape
+        //     : PdfPageFormat.a4.portrait, // Specify A4 landscape format
       );
     }
   } catch (e) {
@@ -51,7 +51,6 @@ Future<void> printDocument(
             transactionData['transactionType'].contains('expenditure')
         ? 1
         : settingInvoiceCopies;
-    final isLandScape = transactionData['transactionType'].contains('Receipt') ? true : false;
     final image = await loadImage('assets/images/invoice_logo.PNG');
     final filePath = gePdfpath('test_file');
     if (context.mounted) {
@@ -60,7 +59,7 @@ Future<void> printDocument(
 
       final file = File(filePath);
       await file.writeAsBytes(await pdf.save());
-      _printPDf(pdf, numCopies, isLandScape);
+      _printPDf(pdf, numCopies);
     }
   } catch (e) {
     debugLog('Pdf creation failed - ($e)');
