e4eab844c3c733e8e315f91564cae61855470807
Shaheen <AiExpert2025@gmail.com>
2025-03-12 13:53:13 +0300

    improve the task tracking screen

--- lib/src/features/daily_tasks/view/tasks_screen.dart
+++ lib/src/features/daily_tasks/view/tasks_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:tablets/src/common/values/constants.dart';
+import 'package:tablets/src/common/values/gaps.dart';
+import 'package:tablets/src/common/widgets/empty_screen.dart';
 import 'package:tablets/src/common/widgets/main_frame.dart';
+import 'package:tablets/src/features/daily_tasks/model/point.dart';
 import 'package:tablets/src/features/daily_tasks/repo/tasks_repository_provider.dart';
 
 class TasksScreen extends ConsumerWidget {
@@ -10,19 +14,100 @@ class TasksScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final supervisorAsyncValue = ref.watch(tasksStreamProvider);
     return AppScreenFrame(
-      buttonsWidget: const TasksFloatingButtons(),
       Container(
         padding: const EdgeInsets.all(0),
         child: supervisorAsyncValue.when(
           data: (supervisors) {
-            return const Text('hi');
+            return supervisors.isEmpty ? const EmptyPage() : SalesPoints(supervisors);
           },
           loading: () => const CircularProgressIndicator(), // Show loading indicator
           error: (error, stack) => Text('Error: $error'), // Handle errors
         ),
       ),
+      buttonsWidget: const TasksFloatingButtons(),
     );
   }
+}
+
+class SalesPoints extends ConsumerWidget {
+  const SalesPoints(this.salesPoints, {super.key});
+  final List<Map<String, dynamic>> salesPoints;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Create a map with unique names as keys and empty lists as values
+    Map<String, List<Map<String, dynamic>>> groupedMap = {
+      for (var name in salesPoints.map((map) => map['salesmanName'] as String).toSet()) name: []
+    };
+
+    // Populate the map with data from the list of maps
+    for (var map in salesPoints) {
+      String name = map['salesmanName'] as String;
+      groupedMap[name]?.add(map);
+    }
+
+    // Convert the map to a list of widgets
+    List<Widget> widgetList = [];
+    groupedMap.forEach((key, value) {
+      widgetList.add(
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 150,
+                padding: const EdgeInsets.all(5),
+                child: Text(
+                  key, // The name as title
+                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              ...value.map((item) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    HorizontalGap.l,
+                    Container(
+                        width: 120,
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          color: Colors.amber,
+                        ),
+                        child: Text(
+                          item['customerName'],
+                          textAlign: TextAlign.center,
+                        )),
+                  ],
+                );
+              }),
+              const Divider(), // Divider between groups
+            ],
+          ),
+        ),
+      );
+    });
+
+    return ListView(
+      children: widgetList,
+    );
+  }
+
+  // Map<String, List<SalesPoint>> getSalesmenPathes(List<Map<String, dynamic>> salesPoints) {
+  //   // Step 1: Create a map with unique names as keys and empty lists as values
+  //   Map<String, List<SalesPoint>> salesmanPathes = {
+  //     for (var name in salesPoints.map((map) => map['name'] as String).toSet()) name: []
+  //   };
+
+  //   // Step 2: Populate the map with data from the list of Sales points
+  //   for (var map in salesPoints) {
+  //     String name = map['name'] as String;
+  //     salesmanPathes[name]?.add(SalesPoint.fromMap(map));
+  //   }
+  //   return salesmanPathes;
+  // }
 }
 
 class TasksFloatingButtons extends ConsumerWidget {
@@ -43,7 +128,31 @@ class TasksFloatingButtons extends ConsumerWidget {
         SpeedDialChild(
           child: const Icon(Icons.add, color: Colors.white),
           backgroundColor: iconsColor,
-          onTap: () {},
+          onTap: () {
+            const salesmanName = '';
+            const salesmanDbRef = '';
+            const customerName = '';
+            const customerDbRef = '';
+            final date = DateTime.now();
+            const isVisited = false;
+            const hasTransaction = false;
+            const dbRef = 'qoiurwwr';
+            final imageUrls = [defaultImageUrl];
+            const name = '';
+            final salespoint = SalesPoint(
+              salesmanName,
+              salesmanDbRef,
+              customerName,
+              customerDbRef,
+              date,
+              isVisited,
+              hasTransaction,
+              dbRef,
+              imageUrls,
+              name,
+            );
+            ref.read(tasksRepositoryProvider).addItem(salespoint);
+          },
         ),
       ],
     );
